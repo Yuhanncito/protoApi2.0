f07877b7cee5e2d3238f984f4f1188d392284e96
Yuhanncito <uchijaisuka02@gmail.com>
2024-01-23 01:36:51 -0600

    confirmEmail2

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,5 @@
 import { Router } from "express";
 const router = Router();
 
-router.get('/',productsController.getProducts)
-router.post('/',productsController.createProduct)
-router.get('/:productId',productsController.getProductById)
-router.put('/:productId',productsController.updateProductById)
-router.delete('/:productId',productsController.deleteProductById)
 
 export default router;
