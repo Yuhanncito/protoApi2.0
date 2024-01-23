6ae844f5706f87a849dfd5eca13f66de7537f72c
Yuhanncito <uchijaisuka02@gmail.com>
2024-01-23 01:54:02 -0600

    confirmEmail3

--- src/middlewares/authEmail.js
+++ src/middlewares/authEmail.js
@@ -10,13 +10,24 @@ export const verifyEmail = async (email,codigoSecreto)=> {
 
         const savedConfirm = newConfirm.save();
 
-        await transporter.sendMail({
-        from: '"Codigo de Verificación" <uchijaisuka02@gmial.com>', // sender address
-        to: email, // list of receivers
-        subject: "Codigo de verificacion", // Subject line
-        text: "Tu codigo de verificacion es:", // plain text body
-        html: `<p>${codigoSecreto}</p>`, // html body
-      });
+        await new Promise ((resolve,reject)=>{
+            transporter.sendMail({
+                from: '"Codigo de Verificación" <uchijaisuka02@gmial.com>', // sender address
+                to: email, // list of receivers
+                subject: "Codigo de verificacion", // Subject line
+                text: "Tu codigo de verificacion es:", // plain text body
+                html: `<p>${codigoSecreto}</p>`, // html body
+            },(err,info)=>{
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                  } else {
+                    console.log(info);
+                    resolve(info);
+                  }
+            })
+        }
+      );
     }catch(err){
         console.log(err);
     }
