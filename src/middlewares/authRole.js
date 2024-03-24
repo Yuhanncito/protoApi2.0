18e21a252dc21eee342923cff091040fd8e30788
Yuhanncito <uchijaisuka02@gmail.com>
2024-03-24 03:12:27 -0600

    authRole

--- src/middlewares/authRole.js
+++ src/middlewares/authRole.js
@@ -8,20 +8,24 @@ export const verifyRole = async (req, res, next) => {
         if (!token) return res.status(403).json({ message: "No token provided" });
 
         const id = await getUserId(token);
-        if (!user) return res.status(404).json({ message: "User not found" });
+        
+        if (!id) return res.status(404).json({ message: "User not found" });
+
 
         const { workspaceid } = req.body;
+
         
         if (!workspaceid) return res.status(400).json({ message: "Workspace ID is required" });
 
         const workspace = await Workspace.findById(workspaceid);
         if (!workspace) return res.status(404).json({ message: "Workspace not found" });
 
-        if (workspace.propetaryUser.toString() !== user._id.toString()) {
+        if (workspace.propetaryUser.toString() !== id._id.toString()) {
+            console.log("usuarios Exactos");
             return res.status(400).json({ message: "User is not authorized for this workspace" });
         }
         next();
     } catch (error) {
-        return res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error role" });
     }
 }
